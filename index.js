ecbf7108c70fc9f4b9d089b8a07dd06047b6d146
sushen123 <sushensame@gmail.com>
2024-01-05 13:23:19 +0545

    Learning javascript foundation

---
+++ index.js
@@ -0,0 +1,25 @@
+//alert ( 3+ 3)) we are using nodejs, not browser
+console.log(3+3);
+console.log("Sushen");
+
+let name = "sushen";
+let age = 18;
+let isLoggedIn = false;
+
+// number => to power 53
+//big int
+//string => ""
+//bolllean => true/false
+//null => standalone value
+//undefined => 
+// symbol unique
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof 18); //number
+console.log(typeof null); // null
+console.log(typeof undefined); //undefined
+
+let score = 33
+
+co
---
+++ index1.js
@@ -0,0 +1,18 @@
+let score = true;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// true =1  false = 0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
